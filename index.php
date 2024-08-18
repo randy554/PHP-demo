a8e94512df6de7e0b58573e39373db7639243b13
Randy <57792277+randy554@users.noreply.github.com>
2024-08-18 23:08:20 +0200

    Added routes V1

--- index.php
+++ index.php
@@ -2,7 +2,18 @@
 
 require "functions.php";
 
-$heading = "Home";
+$uri = parse_url($_SERVER["REQUEST_URI"])["path"];
 
+// Routes
+if ($uri === '/') {
 
-require "views/index.view.php";
+    require "controllers/index.php";
+    
+} else if ($uri === '/about') {
+    
+    require "controllers/about.php";
+
+} elseif ($uri === '/contact') {
+    
+    require "controllers/contact.php";
+}
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -10,8 +10,8 @@
             <div class="ml-10 flex items-baseline space-x-4">
               <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
               <a href="/" class="<?php echo urlIs('/') ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white" <?php if(urlIs('/')) { echo "aria-current='page'";} ?>>Home</a>
-              <a href="/about.php" class="<?php echo urlIs('/about.php') ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white" <?php if(urlIs('/about.php')) { echo "aria-current='page'";} ?>>About</a>
-              <a href="/contact.php" class="<?php echo urlIs('/contact.php') ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" <?php if(urlIs('/contact.php')) { echo "aria-current='page'";} ?>>Contact</a>
+              <a href="/about" class="<?php echo urlIs('/about') ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium hover:bg-gray-700 hover:text-white" <?php if(urlIs('/about')) { echo "aria-current='page'";} ?>>About</a>
+              <a href="/contact" class="<?php echo urlIs('/contact') ? "bg-gray-900 text-white" : "text-gray-300"; ?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" <?php if(urlIs('/contact')) { echo "aria-current='page'";} ?>>Contact</a>
 
             </div>
           </div>
