3173fc94b7e89bbbddb040db5f899a754959b383
Randy <57792277+randy554@users.noreply.github.com>
2024-08-22 08:25:30 +0200

    Added Database Class

---
+++ Database.php
@@ -0,0 +1,26 @@
+<?php
+
+class Database {
+
+public $connection;
+
+public function __construct() {
+    
+    $dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+    $this->connection = new PDO($dsn);
+
+}
+
+public function query ($query) {
+
+    // Connect To My MySQL Database
+
+    // Prepare & Execute SQL Query To Database
+    $statement = $this->connection->prepare($query);
+    $statement->execute();
+
+
+    return $statement;
+
+}
+}
--- index.php
+++ index.php
@@ -4,18 +4,13 @@ require "functions.php";
 
 // require "router.php";
 
+require "Database.php";
 
-// Connect To Our MySQL Database
-$dsn = "mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
-$pdo = new PDO($dsn);
 
-// Prepare & Execute SQL Query To Database
-$statement = $pdo->prepare("SELECT * FROM posts");
-$statement->execute();
+$db = new Database();
+$posts = $db->query("SELECT * FROM posts")->fetchAll(PDO::FETCH_ASSOC);
 
-// Get All The Record Results From The Executed Query
-// ::FETCH_ASSOC To Receive Only Associative Array Results
-$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
 
 
 foreach ($posts as $post) :
