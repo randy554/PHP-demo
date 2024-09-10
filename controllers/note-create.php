47ef0f443ba587d1f5842e5b5469c7463861ea14
Randy <57792277+randy554@users.noreply.github.com>
2024-09-10 02:26:41 +0200

    Added form validation
    - Check if user submission is empty
    - Check if user submission exceeds 1000 characters

--- controllers/note-create.php
+++ controllers/note-create.php
@@ -6,10 +6,30 @@ $db = new Database($config['database']);
 $heading = 'Create Note';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $db->query('INSERT INTO notes (body, user_id) VALUES (?, ?)', [
-        $_POST["body"],
-        1
-    ]);
+
+    // Store potential errors
+    $errors = [];
+
+    // If User Form Submission Is Empty
+    if (strlen($_POST['body']) === 0) {
+        $errors['body'] = 'A body is required';
+    }
+
+    // If User Form Submission Is Too Long
+    if (strlen($_POST['body']) > 1000) {
+        $errors['body'] = 'The body can not be more than 1,000 characters';
+    }
+
+    // If There Are No Form Errors Then Submit Form To DB
+    if (empty($errors)) {
+        
+        $db->query('INSERT INTO notes (body, user_id) VALUES (?, ?)', [
+            $_POST["body"],
+            1
+        ]);
+
+    }
+
 }
 
 require 'views/note-create.view.php';
